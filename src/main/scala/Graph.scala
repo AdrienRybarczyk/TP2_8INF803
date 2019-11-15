7873773341857bd51e5196373234bd3cf2cc55bf
Kragzmo-Simon <fery.simon@ymail.com>
2019-11-15 16:42:14 +0100

    Crawling debut

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -4,9 +4,45 @@ import org.apache.spark.rdd.RDD
 import scala.collection.mutable.ArrayBuffer
 // import classes required for using GraphX
 import org.apache.spark.graphx._
+import org.jsoup.Jsoup
 
 
 object creation extends App{
+
+  def create_monster(monster_url : String, monster_name : String): Monster = {
+    val doc = Jsoup.connect(monster_url).get()
+    val statblock = doc.select(".statblock")
+
+    // HP max
+    var defense_block = statblock.select("p").get(3)
+    if (!(defense_block.html() contains "<b>AC</b>")) {
+      defense_block = statblock.select("p").get(4)
+    }
+    val defense_stats = defense_block.text().split(" |,|;")
+    val defense = defense_stats(1).toInt
+
+    var hp = 0
+    var dr = 0
+    var current_index = 0
+    for (element <- defense_stats) {
+      current_index = defense_stats.indexOf(element)
+      if (element == "hp") {
+        hp = defense_stats(current_index+1).toInt
+      }
+      if (element == "DR") {
+        dr = defense_stats(current_index+1).split("/")(0).toInt
+      }
+    }
+
+    println(dr)
+    //println(hp)
+    //println(defense)
+
+    val new_monster = new Monster(monster_name, hp, hp, defense, dr, attacks, 150, "Gud guys", buffs, 0, 0, 0)
+
+    return new_monster
+  }
+
   val conf = new SparkConf()
     .setAppName("Creation Graphe")
     .setMaster("local[*]")
@@ -24,12 +60,17 @@ object creation extends App{
   var tabMonster = ArrayBuffer[Monster]()
   //val vertices = ArrayBuffer[(VertexId, String)]
 
-  var configurationCombat1 = Array(("Solar",1),("Worgs Rider",9),("Le Warlord",1),("Barbares Orc",4))
+  var configurationCombat1 = Array( ("Solar",1,"https://www.d20pfsrd.com/bestiary/monster-listings/outsiders/angel/solar/"),
+                                    ("Worgs Rider",9,"https://www.d20pfsrd.com/bestiary/npc-s/npcs-cr-1/orc-worg-rider/"),
+                                    ("Le Warlord",1,"https://www.d20pfsrd.com/bestiary/npc-s/npcs-cr-12/brutal-warlord-half-orc-fighter-13/"),
+                                    ("Barbares Orc",4,"https://www.d20pfsrd.com/bestiary/npc-s/npcs-cr-10/double-axe-fury-half-orc-barbarian-11/"))
   var cpt = 1
   val i,j = 0
   for(i <- configurationCombat1.indices){
     for(j <- 0 until configurationCombat1(i)._2){
-      val new_monster = new Monster(configurationCombat1(i)._1 + " " + cpt, 80, 70, 30, 20, attacks, 150, "Gud guys", buffs, 0, 0, 0)
+      val monster_name = configurationCombat1(i)._1 + " " + cpt
+      val new_monster = create_monster(configurationCombat1(i)._3, monster_name)
+
       tabMonster+=new_monster
       //vertices+=(tabVertexID(i),new_monster.name)
       cpt= cpt+1
